e24850359363f37fa73aceaa490631d29f222353
leshina17 <alyhsadmai@gmail.com>
2023-09-28 01:28:25 +0300

    Append 1 file

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    double a,b;
+    scanf("%lf %lf", &a, &b);
+    printf("%lf\n", a+b);
+    printf("%lf\n", a-b);
+    printf("%lf\n", a*b);
+    printf("%lf\n", a/b);
+    return 0;
+}
